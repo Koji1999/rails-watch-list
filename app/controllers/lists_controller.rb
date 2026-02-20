188815ad23b3e328d25e0baa33f6b9ce47551450
Koji Kaga <koji.farre@me.com>
2026-02-20 09:20:24 +0900

    Done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(set_params)
+
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def set_params
+    params.require(:list).permit(:name)
+  end
+end
